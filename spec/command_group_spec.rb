0ea75bf231cc1c069c26e4856eace10fb2b11e9e
Justin Piotroski <justin.piotroski@gmail.com>
2021-01-28 22:19:04 -0800

    Add specs for text blocks

--- spec/command_group_spec.rb
+++ spec/command_group_spec.rb
@@ -1,12 +1,36 @@
 RSpec.describe PiMaker::CommandGroup do
   let(:command_group) { FactoryBot.build(:command_group) }
 
-  describe 'commands' do
+  describe '#commands' do
     subject { command_group.commands }
 
     it 'returns a string array' do
       expect(subject).to be_a(Array)
       expect(subject).to all(be_a(String))
+    end
+  end
+
+  describe '#text_blocks' do
+    subject { command_group.text_blocks }
+
+    it 'returns a filepath => string array hash' do
+      expect(subject).to be_a(Hash)
+
+      expect(subject.keys).to all(be_a(String))
+      expect(subject.keys).to all(be_a_filepath)
+
+      expect(subject.values).to all(be_a(Array))
+      expect(subject.values).to all(all(be_a(String)))
+    end
+  end
+
+  describe 'transformation methods' do
+    it 'responds to all of the Recipe fields' do
+      mtds = PiMaker::Recipe::LISTS.keys + PiMaker::Recipe::TEXT_BLOCKS.keys
+
+      mtds.each do |mtd|
+        expect(command_group).to respond_to(mtd)
+      end
     end
   end
 end
