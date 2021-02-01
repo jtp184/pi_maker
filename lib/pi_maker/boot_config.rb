d43846cda2334797cc60b246cf3e8ebdb018df68
Justin Piotroski <justin.piotroski@gmail.com>
2021-01-31 20:46:42 -0800

    Added boot config class, which wraps options for config.txt

---
+++ lib/pi_maker/boot_config.rb
@@ -0,0 +1,50 @@
+require 'ostruct'
+
+module PiMaker
+  # Wrapper for the config.txt file on the boot volume
+  class BootConfig
+    attr_reader :config, :path
+
+    # Takes in +opts+ for the config and path
+    def initialize(opts = {})
+      @config = OpenStruct.new(
+        opts.fetch(
+          :config,
+          {
+            'dtparam=audio' => 'on',
+            'dtoverlay' => 'vc4-fkms-v3d',
+            'max_framebuffers' => '2'
+          }
+        )
+      )
+
+      @path = opts[:path]
+
+      @path ||= case PiMaker.host_os
+                when :mac
+                  '/Volumes/boot'
+                when :linux, :raspberrypi
+                  '/mnt/boot'
+                else
+                  'E:/'
+                end
+    end
+
+    # Pass arguments to config
+    def method_missing(mtd_name, *args, &blk)
+      config.public_send(mtd_name, *args, &blk)
+    end
+
+    # True if responds to config
+    def respond_to_missing?(mtd_name, _priv = false)
+      config[mtd_name] || super
+    end
+
+    # Output all config options as a stream of k=v
+    def to_s
+      config.to_h.map do |k, v|
+        "#{k}=#{v}"
+      end.join("\n")
+    end
+  end
+end
