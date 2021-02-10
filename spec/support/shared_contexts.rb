69a1997433971c7ff2678a44f86c1018876b046a
Justin Piotroski <justin.piotroski@gmail.com>
2021-02-09 23:58:35 -0800

    Fix stringvars

--- spec/support/shared_contexts.rb
+++ spec/support/shared_contexts.rb
@@ -1,21 +1,21 @@
 RSpec.shared_context 'on windows platform' do
-  before(:each) { stub_const('RUBY_PLATFORM', platform_strings[:windows]) }
+  before(:each) { stub_const('RUBY_PLATFORM', 'mswin') }
 end
 
 RSpec.shared_context 'on mac platform' do
-  before(:each) { stub_const('RUBY_PLATFORM', platform_strings[:mac]) }
+  before(:each) { stub_const('RUBY_PLATFORM', 'darwin') }
 end
 
 RSpec.shared_context 'on linux platform' do
   before(:each) do
-    stub_const('RUBY_PLATFORM', platform_strings[:linux])
+    stub_const('RUBY_PLATFORM', 'linux')
     allow(File).to receive(:read).with('/proc/cpuinfo').and_return('Ubuntu')
   end
 end
 
 RSpec.shared_context 'on raspberrypi platform' do
   before(:each) do
-    stub_const('RUBY_PLATFORM', platform_strings[:raspberrypi])
+    stub_const('RUBY_PLATFORM', 'linux')
     allow(File).to receive(:read).with('/proc/cpuinfo').and_return('Raspberry Pi')
   end
 end
