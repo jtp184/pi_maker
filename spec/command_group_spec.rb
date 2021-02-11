850ef31dadea2268ecdc635cabd26c125481f326
Justin Piotroski <justin.piotroski@gmail.com>
2021-02-11 08:54:32 -0800

    Fix name in command group test

--- spec/command_group_spec.rb
+++ spec/command_group_spec.rb
@@ -25,8 +25,8 @@ RSpec.describe PiMaker::CommandGroup do
   end
 
   describe 'transformation methods' do
-    it 'responds to all of the Recipe fields' do
-      mtds = PiMaker::Recipe::LISTS.keys + PiMaker::Recipe::TEXT_BLOCKS.keys
+    it 'responds to all of the Ingredients fields' do
+      mtds = PiMaker::Ingredients::LISTS.keys + PiMaker::Ingredients::TEXT_BLOCKS.keys
 
       mtds.each do |mtd|
         expect(command_group).to respond_to(mtd)
