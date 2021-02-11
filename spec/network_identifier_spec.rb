e911cfba4b05df0dd8cb5a3e45bf1765ad826b8f
Justin Piotroski <justin.piotroski@gmail.com>
2021-02-10 18:42:10 -0800

    Added test specs expanding on network identifier

--- spec/network_identifier_spec.rb
+++ spec/network_identifier_spec.rb
@@ -1,36 +1,80 @@
 RSpec.describe PiMaker::NetworkIdentifier do
   describe '.call' do
-    subject { described_class.call(scan_with: program) }
+    subject { described_class.call }
 
     before do
       allow(TTY::Which).to(
         receive(:which).with(program.to_s).and_return(!FactoryBot.fixtures[program].nil?)
       )
+
+      allow(PiMaker).to receive(:system_cmd).and_return(FactoryBot.fixtures[program])
     end
 
-    context 'using nmap' do
-      let(:program) { :nmap }
-      before { allow(PiMaker).to receive(:system_cmd).and_return(FactoryBot.fixtures[:nmap]) }
-
-      it 'returns a list of ips' do
-        expect(subject).to all(be_a(String))
-      end
-
-      it 'ignores non-pi hosts' do
-        expect(subject).to eq(['192.168.1.187'])
-      end
-    end
-
-    context 'using arp' do
+    context 'with no provided scan_with program' do
       let(:program) { :arp }
-      before { allow(PiMaker).to receive(:system_cmd).and_return(FactoryBot.fixtures[:arp]) }
 
-      it 'returns a list of ips' do
-        expect(subject).to all(be_a(String))
+      context 'with a program installed' do
+        it 'runs the command' do
+          expect { subject }.not_to raise_error
+        end
+
+        it { is_expected.to eq(['192.168.1.187']) }
+      end
+    end
+
+    context 'providing a scan_with program' do
+      subject { described_class.call(scan_with: program) }
+
+      context 'using nmap' do
+        let(:program) { :nmap }
+
+        it 'returns a list of ips' do
+          expect(subject).to all(be_a(String))
+        end
+
+        it 'ignores non-pi hosts' do
+          expect(subject).to eq(['192.168.1.187'])
+        end
       end
 
-      it 'ignores non-pi hosts' do
-        expect(subject).to eq(['192.168.1.187'])
+      context 'using arp' do
+        let(:program) { :arp }
+
+        it 'returns a list of ips' do
+          expect(subject).to all(be_a(String))
+        end
+
+        it 'ignores non-pi hosts' do
+          expect(subject).to eq(['192.168.1.187'])
+        end
+      end
+
+      context 'using noninstalled program' do
+        let(:program) { :lookit }
+
+        it 'raises an error' do
+          expect { subject }.to raise_error(ArgumentError)
+        end
+
+        context 'providing procs to use the program' do
+          let(:program) { :echo }
+          let(:run_with_proc) { ->(i) { i } }
+          let(:parse_with_proc) { ->(i) { i } }
+          let(:filter_with_proc) { ->(i) { i } }
+
+          subject do
+            described_class.call(
+              scan_with: program,
+              run_with: run_with_proc,
+              filter_with: filter_with_proc,
+              parse_with: parse_with_proc
+            )
+          end
+
+          it 'uses the procs to run the program' do
+            expect { subject }.not_to raise_error
+          end
+        end
       end
     end
   end
