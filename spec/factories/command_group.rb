fd416261755419a9d55f55cecb74dee02181ec19
Justin Piotroski <justin.piotroski@gmail.com>
2021-03-04 22:02:42 -0800

    Fix initialization

--- spec/factories/command_group.rb
+++ spec/factories/command_group.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :command_group, class: PiMaker::CommandGroup do
     instructions
 
-    initialize_with { PiMaker::CommandGroup.new(**attributes) }
+    initialize_with { PiMaker::CommandGroup.new(instructions) }
   end
 end
