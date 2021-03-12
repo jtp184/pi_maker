6fe97403f878499f33f8512c15f9138aa9ac0b29
Justin Piotroski <justin.piotroski@gmail.com>
2021-03-11 22:58:01 -0800

    Config method option for up and download

--- lib/pi_maker/commands/remote.rb
+++ lib/pi_maker/commands/remote.rb
@@ -19,6 +19,8 @@ module PiMaker
       end
 
       desc 'upload', 'Transfer a local file to the remote'
+      method_option :config, aliases: '-c', type: :hash, default: nil,
+                             desc: 'Pass in a hash of config options'
 
       def upload(local_file = nil, remote_file = nil)
         if options[:help]
@@ -30,6 +32,8 @@ module PiMaker
       end
 
       desc 'download', 'Copy a remote file'
+      method_option :config, aliases: '-c', type: :hash, default: nil,
+                             desc: 'Pass in a hash of config options'
 
       def download(remote_file = nil, local_file = nil)
         if options[:help]
